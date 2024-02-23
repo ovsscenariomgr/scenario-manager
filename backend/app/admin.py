7c15364900fcc7c94c0adaa8c5ded6183e0b3603
David Haak <dh749@cornell.edu>
2024-02-23 10:09:11 -0700

    Finally saving the whole model, with sane defaults. Great success.

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,31 +1,48 @@
 from django.contrib import admin
 import nested_admin
-
-# Register your models here.
+from .forms import TitleForm, AvatarForm, SummaryForm, ControlForm
 from .models import *
 
 class TitleInline(nested_admin.NestedStackedInline):
     model = Title
+    form = TitleForm
+    classes = ('grp-collapse grp-open',) # Set so default is form opened.
+    inline_classes = ('grp-collapse grp-open',)
 
 class HeaderInline(nested_admin.NestedStackedInline):
     model = Header
-    inlines = [TitleInline]
+    inlines = [TitleInline,]
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
 
 class AvatarInline(nested_admin.NestedStackedInline):
     model = Avatar
+    form = AvatarForm
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
 
 class SummaryInline(nested_admin.NestedStackedInline):
     model = Summary
+    form = SummaryForm
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
 
 class ControlInline(nested_admin.NestedStackedInline):
     model = Control
+    form = ControlForm
+    extra = 1
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
 
 class ProfileInline(nested_admin.NestedStackedInline):
     model = Profile
-    inlines = [AvatarInline, SummaryInline, ControlInline]
+    inlines = [AvatarInline, SummaryInline, ControlInline,]
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
 
+@admin.register(Scenario)
 class ScenarioAdmin(nested_admin.NestedModelAdmin):
     model = Scenario
-    inlines = [HeaderInline, ProfileInline]
-
-admin.site.register(Scenario, ScenarioAdmin)
+    inlines = [HeaderInline, ProfileInline,]
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
